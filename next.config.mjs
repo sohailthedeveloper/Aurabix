f90f87a88182a7c3e99d1927df753b4c45ce8ff0
Sohail Shaikh <hello@aurabix.com>
2026-09-15 08:43:51 +0530

    chore: setup 301 permanent redirects for deprecated routes to preserve SEO

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,29 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/founder',
+        destination: '/#team',
+        permanent: true,
+      },
+      {
+        source: '/pricing',
+        destination: '/#pricing',
+        permanent: true,
+      },
+      {
+        source: '/demo',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/generator',
+        destination: '/',
+        permanent: true,
+      }
+    ];
+  },
 };
 
 export default nextConfig;
